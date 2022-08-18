7aab0c55eb67f0a825fad31984df9f014a4c02e0
PharmCat <13901158+PharmCat@users.noreply.github.com>
2022-08-18 16:26:51 +0300

    update

--- src/qmcint.jl
+++ src/qmcint.jl
@@ -24,19 +24,8 @@ end
 Simple Quasi-Monte Carlo integration.
 """
 
-function qmcintegral(f::Function, a::AbstractVector, b::AbstractVector, sampler::QuasiMonteCarlo.SamplingAlgorithm  = SobolSample(); n::Int = 1000, rng = RandomDevice())
-    s   = 0.0
-    ssq = 0.0
-    dim = length(a)
-    spl = QuasiMonteCarlo.sample(n, a, b, sampler)
-    for i = 1:n
-        v    = f(view(spl, :, i))
-        s   += v
-        ssq += v*v
-    end
-    r = prod(b-a)*s/n
-    e = sqrt(prod(b-a)*ssq/n - prod(b-a)*(s/n)^2)/sqrt(n-1)
-    r, e
+function qmcintegral(f::Function, a::AbstractVector, b::AbstractVector, sampler  = SobolSample(); kwargs...)
+    qmcintegral(f, a, b, sampler; kwargs...)
 end
 
 function qmcintegral(f::Function, a::AbstractVector, b::AbstractVector, sampler::SobolSample; n::Int = 1000, rng = RandomDevice())
@@ -48,6 +37,21 @@ function qmcintegral(f::Function, a::AbstractVector, b::AbstractVector, sampler:
     for i = 1:n
         QuasiMonteCarlo.next!(smplr, xvec)
         v    = f(xvec)
+        s   += v
+        ssq += v*v
+    end
+    r = prod(b-a)*s/n
+    e = sqrt(prod(b-a)*ssq/n - prod(b-a)*(s/n)^2)/sqrt(n-1)
+    r, e
+end
+
+function qmcintegral(f::Function, a::AbstractVector, b::AbstractVector, sampler::QuasiMonteCarlo.SamplingAlgorithm; n::Int = 1000, rng = RandomDevice())
+    s   = 0.0
+    ssq = 0.0
+    dim = length(a)
+    spl = QuasiMonteCarlo.sample(n, a, b, sampler)
+    for i = 1:n
+        v    = f(view(spl, :, i))
         s   += v
         ssq += v*v
     end
